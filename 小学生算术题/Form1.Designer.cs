5ebb9d06b7281fa2c86d01b086cb336d0fd697cf
SoftStar <qq764073542@163.com>
2015-10-24 19:49:29 +0800

    随机生成计算题

---
+++ 小学生算术题/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace 小学生算术题
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "生成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "比对答案";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(362, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "查看答案";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(91, 21);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "输入生成题目的数量：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "小学算术题";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(460, 47);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "自动填充测试数据";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(361, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "正确率统计：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(460, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(598, 433);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "CSharp作业-小学算数题";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 小学生算术题/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 小学生算术题
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //定义为全局变量，一遍其他函数调用
+        List<int> list_result = new List<int>(); //保存算数结果
+        List<PictureBox> list_pic = new List<PictureBox>(); //保存结果对错对应的pic对象
+        List<TextBox> list_input = new List<TextBox>(); //保存用户输入数据对应的textbox集合   
+        List<Label> list_result_lable = new List<Label>(); //保存显示结果的label对象
+        int right_rate = 0;
+        int topic_size = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                topic_size = Int32.Parse(textBox1.Text.Trim());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("输入有误，将默认输出10道题目");
+                topic_size = 10;
+            }
+            
+            Random random = new Random(1000); //随机数种子            
+            for (int i = 1; i <= topic_size; i++)
+            {
+                Label label_operand1 = new Label(); //运算数01
+                Label label_0perator = new Label(); //运算符
+                Label label_operand2 = new Label(); //运算数02
+                Label label_equal = new Label(); //运算符 = 
+                TextBox textBox = new TextBox(); // 用户输入的结果
+                PictureBox pictureBox = new PictureBox(); //用户输入正误对应的pic
+                Label label_result = new Label(); //结果显示label                
+             
+                DateTime date = DateTime.Now; //获取系统当前时间，添加时间变量，保证随机数不重复
+                int num1 = (random.Next(1, 1000) + date.Millisecond) % 100;//生成1-100的随机数
+                int num2 = (random.Next(1, 1000) + date.Millisecond) % 100;
+                int Operator = (random.Next(1, 10) + date.Millisecond) % 2; //生成1/2的随机数来判断操作数
+
+                //label_operand1
+                label_operand1.AutoSize = true;
+                label_operand1.Location = new System.Drawing.Point(40, 60 +(30* i)); //列布局               
+                label_operand1.Size = new System.Drawing.Size(30, 12);
+                label_operand1.TabIndex = 0;
+                label_operand1.Text = num1 + "";
+
+                //label_0perator
+                label_0perator.AutoSize = true;
+                label_0perator.Location = new System.Drawing.Point(80, 60 + (30 * i));                
+                label_0perator.Size = new System.Drawing.Size(30, 12);
+                label_0perator.TabIndex = 0;
+                if (Operator % 2 == 0)
+                {
+                    label_0perator.Text = "+";
+                    list_result.Add(num1 + num2);
+                }
+                else
+                {
+                    label_0perator.Text = "-";
+                    list_result.Add(num1 - num2);
+                }                
+
+                //label_operand2
+                label_operand2.AutoSize = true;
+                label_operand2.Location = new System.Drawing.Point(120, 60 + (30 * i));               
+                label_operand2.Size = new System.Drawing.Size(30, 12);
+                label_operand2.TabIndex = 0;
+                label_operand2.Text = num2 + "";
+
+                //label_equal
+                label_equal.AutoSize = true;
+                label_equal.Location = new System.Drawing.Point(160, 60 + (30 * i));              
+                label_equal.Size = new System.Drawing.Size(30, 12);
+                label_equal.TabIndex = 0;
+                label_equal.Text = "=";
+
+                //textBox           
+                textBox.Location = new System.Drawing.Point(200, 55 + (30 * i));                
+                textBox.Size = new System.Drawing.Size(30, 20);
+                textBox.TabIndex = 3;
+                list_input.Add(textBox);
+
+                //pictureBox        
+                pictureBox.Location = new System.Drawing.Point(240, 50 + (30 * i));                
+                pictureBox.Size = new System.Drawing.Size(30, 30);
+                pictureBox.TabIndex = 5;
+                pictureBox.TabStop = false;               
+                list_pic.Add(pictureBox);
+
+                //label_result
+                label_result.AutoSize = true;
+                label_result.Location = new System.Drawing.Point(280, 60 + (30 * i));
+                label_result.Size = new System.Drawing.Size(30, 12);
+                label_result.TabIndex = 0;
+                list_result_lable.Add(label_result);
+         
+
+                //将创建的空间添加到布局上
+                this.Controls.Add(label_operand1);
+                this.Controls.Add(label_0perator);
+                this.Controls.Add(label_operand2);
+                this.Controls.Add(label_equal);
+                this.Controls.Add(textBox);
+                this.Controls.Add(pictureBox);
+                this.Controls.Add(label_result);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            right_rate = 0;
+             for(int i=0;i<list_input.Count;i++)
+            {
+                try
+                {
+                    int num = Int32.Parse(list_input[i].Text.Trim());
+
+                    if (list_result[i] == num) //表示当前本条数据计算正确
+                    {
+                        list_pic[i].ImageLocation = System.Windows.Forms.Application.StartupPath + @"\\right.png";
+                        right_rate++;
+                    }
+                    else
+                    {
+                        list_pic[i].ImageLocation = System.Windows.Forms.Application.StartupPath + @"\\fault.png";
+                    }
+
+                }
+                catch (Exception)
+                {
+                    list_pic[i].ImageLocation = System.Windows.Forms.Application.StartupPath + @"\\fault.png";
+                }                
+
+            }
+             label4.Text = ((float)right_rate/topic_size)*100 +"%";
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for(int i=0;i<list_result_lable.Count;i++)
+            {
+                list_result_lable[i].Text = list_result[i] + "";
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for(int i=0;i<list_input.Count;i++)
+            {
+                list_input[i].Text = list_result[i].ToString();
+            }
+            
+        }
+    }
+}
